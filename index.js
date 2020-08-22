ace5a00d5c30f6b665d3865c9367661d6c825343
redazures <jwu_nyc@yahoo.com>
2020-08-22 19:14:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    let sank =[]
+    for (let i = 0; i < array.length; i++) {
+        sank.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+      }
+      return sank;
+  }
+
+  function countDown (number){
+    while ( number > 0 ) {
+        console.log( number );
+        number--;
+      }
+      console.log( number);
+  }
