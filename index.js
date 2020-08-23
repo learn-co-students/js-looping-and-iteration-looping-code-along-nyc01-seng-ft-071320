81fcefe04216b092f593135a2fcf8b58fa2b806a
amitdesh <amitdesh2014@gmail.com>
2020-08-23 18:47:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+
+function writeCards(gifts) {
+    let array = []
+    for (let i = 0; i < gifts.length; i++) {
+    array.push(`Thank you, ${gifts[i]}, for the wonderful surprise gift!`);
+    }
+    return array
+}
+
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown= countDown -1);
+    }
+}
